7631dfb0043cce2d19ab4b7717407d68266f7e40
Naveen M K <naveen@syrusdark.website>
2020-09-17 22:51:50 +0530

    [installer] Don't remove anything installer

--- texlive/tools/chocolateyinstall.ps1
+++ texlive/tools/chocolateyinstall.ps1
@@ -34,8 +34,8 @@ $profilelocation = Write-Profile @profileArgs
 
 # extract install.zip
 Get-ChocolateyUnzip -FileFullPath "$toolsDir\install-tl.zip" -Destination "$toolsDir\installer"
-Move-Item "$toolsDir\installer\install-tl-*\*" "$toolsDir"
-Remove-Item "$toolsDir\installer" -Recurse
+Move-Item "$toolsDir\installer\install-tl-*" "$toolsDir"
+#Remove-Item "$toolsDir\installer" -Recurse
 tree
 # This also works for cmd and is required if you have any spaces in the paths within your command
 $appPath = $toolsDir
