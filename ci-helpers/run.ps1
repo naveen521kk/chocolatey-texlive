375038b897357cf65079fb2b2f2e90ac7c695644
Naveen M K <naveen@syrusdark.website>
2020-09-17 20:51:00 +0530

    [ci] correct path

--- ci-helpers/run.ps1
+++ ci-helpers/run.ps1
@@ -1,5 +1,5 @@
 #First Get install-tl thanks texlive.info
-Set-Location texlive
+Set-Location ../texlive
 Invoke-WebRequest -Uri "https://texlive.info/tlnet-archive/2020/09/17/tlnet/install-tl.zip" -OutFile "install-tl.zip"
 Set-Location ../
 
