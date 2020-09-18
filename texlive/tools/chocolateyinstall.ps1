e4926db0022e4fd592e51594cbf0b1e8120ea023
Naveen M K <naveen@syrusdark.website>
2020-09-18 11:54:57 +0530

    [installer]Try powershell alias

--- texlive/tools/chocolateyinstall.ps1
+++ texlive/tools/chocolateyinstall.ps1
@@ -45,5 +45,6 @@ tree $toolsDir
 echo "$(gc $($profilelocation.profileLoc))"
 # This also works for cmd and is required if you have any spaces in the paths within your command
 $cmdBatch = "/c `"`"$($toolsDir)\install-tl-windows.bat`" -no-gui -profile='$($profilelocation.profileLoc)'`""
-Start-ChocolateyProcessAsAdmin -Statements "$cmdBatch" -ExeToRun "cmd.exe" -NoSleep -Elevated -WorkingDirectory "$toolsDir"
+#Start-ChocolateyProcessAsAdmin -Statements "$cmdBatch" -ExeToRun "cmd.exe" -NoSleep -Elevated -WorkingDirectory "$toolsDir"
+Start-ChocolateyProcessAsAdmin "& '$($toolsDir)\install-tl-windows.bat' -no-gui -profile='$($profilelocation.profileLoc)'" -Elevated -WorkingDirectory "$toolsDir"
 
