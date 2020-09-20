8607d2b238997cfd3252b5fd19b71275a00dc644
Naveen M K <naveen@syrusdark.website>
2020-09-20 09:32:07 +0530

    [installer] Double quotes

--- texlive/tools/chocolateyinstall.ps1
+++ texlive/tools/chocolateyinstall.ps1
@@ -41,4 +41,5 @@ Move-Item -Path "$toolsDir\install-tl-*\*" -Destination "$toolsDir" -Force
 #$cmdBatch = "/c `"`"$($toolsDir)\install-tl-windows.bat`" -no-gui -profile='$($profilelocation.profileLoc)'`""
 #Start-ChocolateyProcessAsAdmin -Statements "$cmdBatch" -ExeToRun "cmd.exe" -NoSleep -Elevated -WorkingDirectory "$toolsDir"
 #Start-ChocolateyProcessAsAdmin "& '$($toolsDir)\install-tl-windows.bat' -no-gui -profile='$($profilelocation.profileLoc)'" -Elevated -WorkingDirectory "$toolsDir"
-& '$($toolsDir)\install-tl-windows.bat' -no-gui -profile='$($profilelocation.profileLoc)'
+#Start-ChocolateyProcessAsAdmin  "cmd.exe /C "'$($toolsDir)\install-tl-windows.bat' -no-gui -profile='$($profilelocation.profileLoc)' -q"  -Elevated -WorkingDirectory "$toolsDir"
+& "'$($toolsDir)\install-tl-windows.bat' -no-gui -profile='$($profilelocation.profileLoc)'"
